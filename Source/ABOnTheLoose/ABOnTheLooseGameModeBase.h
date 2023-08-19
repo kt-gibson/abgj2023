8643d6a97287c979c398fd30239cfc8309988270
kt-gibson <77365297+kt-gibson@users.noreply.github.com>
2023-08-18 21:45:42 -0600

    Initial Commit

---
+++ Source/ABOnTheLoose/ABOnTheLooseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ABOnTheLooseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABONTHELOOSE_API AABOnTheLooseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
